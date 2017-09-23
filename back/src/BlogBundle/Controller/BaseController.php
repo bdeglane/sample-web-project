ac3637acb6279b5ad222b4b400722703ac9a3901
Benoit Deglane <benoit.deglane@netapsys.fr>
2017-09-24 00:18:24 +0200

    clean

--- back/src/BlogBundle/Controller/BaseController.php
+++ back/src/BlogBundle/Controller/BaseController.php
@@ -2,85 +2,29 @@
 
 namespace BlogBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Tests\Templating\Fixture\BarBundle\Controller\BarController;
+use ApiBundle\Services\SerializerService;
+use ApiBundle\Services\ViewService;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Serializer\Serializer;
-use Symfony\Component\Serializer\Encoder\XmlEncoder;
-use Symfony\Component\Serializer\Encoder\JsonEncoder;
-use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
-
 
 class BaseController extends Controller
 {
-
-    const JSON = 'json';
+    /**
+     * @var SerializerService
+     */
+    protected $serializer;
+    /**
+     * @var ViewService
+     */
+    protected $view;
 
     /**
-     * @return array
+     * BaseController constructor.
+     * @param SerializerService $serializer
+     * @param ViewService $view
      */
-    private function getNormalizers(): array
+    public function __construct(SerializerService $serializer, ViewService $view)
     {
-        $normalizers = [new ObjectNormalizer()];
-        return $normalizers;
-    }
-
-    /**
-     * @return array
-     */
-    private function getEncoders(): array
-    {
-        $encoders = [new XmlEncoder(), new JsonEncoder()];
-        return $encoders;
-    }
-
-    /**
-     * @return Serializer
-     */
-    private function getSerializer(): Serializer
-    {
-        $normalizers = $this->getNormalizers();
-        $encoders = $this->getEncoders();
-        return new Serializer($normalizers, $encoders);
-    }
-
-    /**
-     * @param $entity
-     * @param $format
-     * @return string
-     */
-    private function serialize($entity, $format)
-    {
-        return $this->getSerializer()->serialize($entity, $format);
-    }
-
-    /**
-     * @param $entity
-     * @param $object
-     * @param $format
-     * @return object
-     */
-    private function deserialize($entity, $object, $format)
-    {
-        return $this->getSerializer()->deserialize($entity, $object, $format);
-
-    }
-
-    /**
-     * @param $entity
-     * @return string
-     */
-    protected function jsonSerialize($entity)
-    {
-        return $this->serialize($entity, BaseController::JSON);
-    }
-
-    /**
-     * @param $string
-     * @param $object
-     * @return string
-     */
-    protected function jsonDeserialize($string, $object)
-    {
-        return $this->deserialize($string, $object, BaseController::JSON);
+        $this->serializer = $serializer;
+        $this->view = $view;
     }
 }
--- back/src/BlogBundle/Controller/DefaultController.php
+++ back/src/BlogBundle/Controller/DefaultController.php
@@ -15,9 +15,15 @@ class DefaultController extends BaseController
         $post->setCreatedAt(new \DateTime());
         $post->setUpdatedAt(new \DateTime());
 
-        $json = $this->jsonSerialize($post);
-        $object = $this->jsonDeserialize($json, Post::class);
+        $json = $this->serializer->jsonSerialize($post);
+        $object = $this->serializer->jsonDeserialize($json, Post::class);
 
-        return $this->render('BlogBundle:Default:index.html.twig', ['json' => $json, 'object' => $object]);
+        $view = $this->view->build();
+        $view->setData($post);
+        $view->setStatus($this->view->getHttpCode()::SUCCESS);
+
+        $jsonView = $this->serializer->jsonSerialize($view);
+
+        return $this->render('BlogBundle:Default:index.html.twig', ['json' => $json, 'object' => $object, 'view' => $view, 'jsonView' => $jsonView]);
     }
 }
